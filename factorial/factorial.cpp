55e52f69c30fe080658575934a75d14426d0d301
Juan Osorio <js.osoriov@uniandes.edu.co>
2020-05-29 20:39:09 -0400

    HW08

--- factorial/factorial.cpp
+++ factorial/factorial.cpp
@@ -1,1 +1,20 @@
 #include <iostream>
+using namespace std;
+//Calcular factorial con recursion
+int factorial(int n)
+{
+   if (n <= 1)
+        return 1;
+   else 
+       return n*factorial(n-1);
+}
+
+int main(){
+    
+   int num;
+   cout << "Introduzca un numero";
+   cin >> num;
+   cout << "Factorial of entered number: " << factorial(num);
+    
+   return 0;
+}
